dae48677721089b9f822cebec8756d8f01d0850f
cdelorey <cdelorey@fake.com>
2014-06-28 19:57:43 -0300

    Connect to Trello

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 ruby '2.1.1'
 
 gem 'sinatra'
+gem 'ruby-trello'
 
 group :development, :test do 
   gem 'rspec'
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,9 +1,17 @@
 require File.expand_path '../spec_helper.rb', __FILE__
-
+require 'trello'
 
 describe "Trello-Todo App" do 
   it "allows accessing the main page" do
     get '/'
     expect(last_response).to be_ok
+  end
+
+  it "connects to Trello" do
+    me = Member.find("me")
+    expect(me).to respond_to(:username)
+  end
+
+  it "connects to Todoist" do
   end
 end
