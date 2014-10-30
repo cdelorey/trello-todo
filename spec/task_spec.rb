9f179a665e7a279f9bb731c8e7ba49cb3eaf5468
cdelorey <cdelorey@users.noreply.github.com>
2014-10-30 18:26:50 -0300

    Fix tests

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -13,7 +13,7 @@ describe Task do
     let(:task_names) { tasks.map { |task| task.content } }
 
     it "gets correct cards" do
-      expect(task_names).to include("Eloquent Ruby")
+      expect(task_names).to include("Learn SQL the Hard Way")
     end
 
     it "gets cards from correct project" do
